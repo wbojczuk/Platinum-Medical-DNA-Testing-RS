e370318069110d35113cd7b33c6e3efa739bd083
William Bojczuk <williambojczuk@gmail.com>
2024-11-18 23:01:20 -0500

    try'

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import ContactInfo from "@/app/(mainsite)/components/contactpage/ContactInfo/ContactInfo"
 import FormAndMap from "@/app/(mainsite)/components/contactpage/FormAndMap/FormAndMap"
 import PageTitle from "@/app/(mainsite)/components/misc/PageTitle/PageTitle"
